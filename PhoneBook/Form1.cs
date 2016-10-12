a16ffc3adedd433e18861ff8afbb7e648b8c67ad
DeronLee <akagilnc@gmail.com>
2016-10-12 22:07:27 +0900

    add query button and add test button. replace the insert to insert or replace

--- PhoneBook/Form1.cs
+++ PhoneBook/Form1.cs
@@ -20,33 +20,23 @@ namespace PhoneBook
 
         private void button1_Click(object sender, EventArgs e)
         {
-            SQLiteConnection myConnection = new SQLiteConnection(@"Data Source = C:\Users\lnc\Documents\Visual Studio 2015\Projects\PhoneBook\PhoneBook.db");
+          
 
-            SQLiteDataAdapter ad;
-            DataTable dt = new DataTable();
+            MessageBox.Show(GetInfo());
+            //MessageBox.Show(Insert().ToString());
 
-            try
-            {
-                SQLiteCommand sqLiteCommand;
-                myConnection.Open();
-                sqLiteCommand = myConnection.CreateCommand();
-                sqLiteCommand.CommandText = "select * from phonebook where name = '" + textBox1.Text + "'";
-                sqLiteCommand.CommandTimeout = 15;
-                sqLiteCommand.CommandType = CommandType.Text;
-                ad = new SQLiteDataAdapter(sqLiteCommand);
-                ad.Fill(dt);
-            }
-            catch (SQLiteException)
-            {
+            
+         
+        }
 
-                throw;
-            }
-            finally
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable result = Insert();
+            MessageBox.Show(result.ToString());
+            for (int i = 0; i < result.Rows.Count; i++)
             {
-                myConnection.Close();
+                MessageBox.Show(result.Rows[i].ToString());
             }
-            Console.Write(dt.Rows);
-            MessageBox.Show("Name:" + dt.Rows[0]["Name"] + "    PhoneNum:" + dt.Rows[0]["PhoneNum"]);
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -54,6 +44,56 @@ namespace PhoneBook
             //MessageBox.Show(e.ToString());
             String name = textBox1.Text;
         }
+
+   
+
+        private String GetInfo()
+        {
+            String sql = "select * from phonebook where name = '" + textBox1.Text + "'";
+            
+
+            DataTable result = DataConnection(sql);
+            return "Name:" + result.Rows[0]["Name"] + "    PhoneNum:" + result.Rows[0]["PhoneNum"];
+        }
+
+        private DataTable Insert()
+        {
+            String sql_insert = "insert or replace into phonebook values(6, 'test4', 18000000000)";
+            DataTable result = DataConnection(sql_insert);
+            return result;
+        }
+
+        private DataTable DataConnection(String SqlCommand)
+        {
+            
+            SQLiteConnection myConnection = new SQLiteConnection(@"Data Source = C:\Users\lnc\Documents\Visual Studio 2015\Projects\PhoneBook\PhoneBook.db");
+            SQLiteDataAdapter ad;
+            DataTable dt = new DataTable();
+            SQLiteTransaction transaction = myConnection.BeginTransaction();
+
+            try
+            {
+                myConnection.Open();
+                SQLiteCommand sqLiteCommand = myConnection.CreateCommand();
+                sqLiteCommand.CommandText = SqlCommand;
+                sqLiteCommand.CommandTimeout = 15;
+                sqLiteCommand.CommandType = CommandType.Text;
+                ad = new SQLiteDataAdapter(sqLiteCommand);
+                ad.Fill(dt);
+                transaction.Commit();
+            }
+            catch (SQLiteException)
+            {
+                transaction.Rollback();
+                throw;
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            return dt;
+        }
+
     }
 }
         
