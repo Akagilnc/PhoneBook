f2059e35031abd79eaf195bc07454f4b924c8b27
DeronLee <akagilnc@gmail.com>
2016-10-15 19:12:20 +0800

    add tooltip to display information when user click on

--- PhoneBook/Index.Designer.cs
+++ PhoneBook/Index.Designer.cs
@@ -28,16 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.querybutton = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.addtestbutton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // querybutton
             // 
-            this.querybutton.Location = new System.Drawing.Point(104, 161);
+            this.querybutton.Location = new System.Drawing.Point(104, 149);
             this.querybutton.Name = "querybutton";
-            this.querybutton.Size = new System.Drawing.Size(75, 23);
+            this.querybutton.Size = new System.Drawing.Size(75, 21);
             this.querybutton.TabIndex = 0;
             this.querybutton.Text = "Query";
             this.querybutton.UseVisualStyleBackColor = true;
@@ -45,32 +50,66 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(91, 135);
+            this.textBox1.Location = new System.Drawing.Point(91, 125);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // addtestbutton
             // 
-            this.addtestbutton.Location = new System.Drawing.Point(104, 191);
+            this.addtestbutton.Location = new System.Drawing.Point(104, 176);
             this.addtestbutton.Name = "addtestbutton";
-            this.addtestbutton.Size = new System.Drawing.Size(75, 23);
+            this.addtestbutton.Size = new System.Drawing.Size(75, 21);
             this.addtestbutton.TabIndex = 2;
             this.addtestbutton.Text = "Add test";
             this.addtestbutton.UseVisualStyleBackColor = true;
             this.addtestbutton.Click += new System.EventHandler(this.testaddbutton_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(47, 19);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 3;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("SimSun", 12F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(54, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click Me";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.toolTip1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.toolTip1.OwnerDraw = true;
+            this.toolTip1.ShowAlways = true;
+            this.toolTip1.ToolTipTitle = "Moji means character";
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            this.toolTip1.Draw += new System.Windows.Forms.DrawToolTipEventHandler(this.toolTip1_Draw);
+            // 
             // Index
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(284, 241);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.addtestbutton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.querybutton);
             this.Name = "Index";
             this.Text = "Index";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +120,9 @@
         private System.Windows.Forms.Button querybutton;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button addtestbutton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
     }
 }
 
