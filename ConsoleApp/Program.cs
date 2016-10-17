813313c234b9028639418f59cb3eb8c7b5101f7e
DeronLee <akagilnc@gmail.com>
2016-10-17 15:54:44 +0900

    add WCF example

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ConsoleApp.ServiceReference1;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Begin========");
+            var client = new Service1Client();
+            var time = client.GetServerTime();
+            var name = client.GetServerName();
+            
+            Console.WriteLine(time + "   " + name);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Contracts/GetServerInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Contracts
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GetServerInfo" in both code and config file together.
+    public class GetServerInfo : IGetServerInfo
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public string GetServerTime()
+        {
+            return DateTime.Now.ToString();
+        }
+
+        public string GetServerName()
+        {
+            return "TestServer";
+        }
+    }
+}
